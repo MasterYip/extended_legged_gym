fdb9963217211d27fbd1c8e099d1e87690237e70
MasterYip-R9000P <raymon-yip@qq.com>
2026-06-24 16:13:09 +0800

    chore: update tripod2_low

--- legged_gym/legged_gym/envs/el_4090/spider_nomal/el4090_tripod2_low_config.py
+++ legged_gym/legged_gym/envs/el_4090/spider_nomal/el4090_tripod2_low_config.py
@@ -8,7 +8,7 @@ class El4090Tripod2LowCfg(El4090SpiderCfg):
     """EL4090 2-group tripod gait on flat terrain — low crouching stance."""
 
     class init_state(El4090SpiderCfg.init_state):
-        pos = [0.0, 0.0, 0.33]
+        pos = [0.0, 0.0, 0.4]
         default_joint_angles = {  # = target angles [rad] when action = 0.0
             "RF_HAA": 0.0,
             "RM_HAA": 0.0,
@@ -17,24 +17,24 @@ class El4090Tripod2LowCfg(El4090SpiderCfg):
             "LM_HAA": 0.0,
             "LB_HAA": 0.0,
 
-            "RF_HFE": 0.6,
-            "RM_HFE": 0.6,
-            "RB_HFE": 0.6,
-            "LF_HFE": 0.6,
-            "LM_HFE": 0.6,
-            "LB_HFE": 0.6,
+            "RF_HFE": 0.0,
+            "RM_HFE": 0.0,
+            "RB_HFE": 0.0,
+            "LF_HFE": 0.0,
+            "LM_HFE": 0.0,
+            "LB_HFE": 0.0,
 
-            "RF_KFE": -1.2,
-            "RM_KFE": -1.2,
-            "RB_KFE": -1.2,
-            "LF_KFE": -1.2,
-            "LM_KFE": -1.2,
-            "LB_KFE": -1.2,
+            "RF_KFE": -0.0,
+            "RM_KFE": -0.0,
+            "RB_KFE": -0.0,
+            "LF_KFE": -0.0,
+            "LM_KFE": -0.0,
+            "LB_KFE": -0.0,
         }
 
     class rewards(El4090SpiderCfg.rewards):
         max_contact_force = 500.
-        base_height_target = 0.22
+        base_height_target = 0.25
         only_positive_rewards = False
         # Multi-stage
         # Stage 0: Learn to walk with tripod gait (with / w\o actuator net)
@@ -54,7 +54,7 @@ class El4090Tripod2LowCfg(El4090SpiderCfg):
             torques = -0.0001
             dof_vel = [-0.0001, -0.0001]
             dof_acc = [-5e-8]
-            base_height = [-2.0, -0.4]
+            base_height = [-2.0, -5.0]
             feet_slip = [-0.0, -0.2]
             feet_air_time = [0.5, 0.1]
             collision = -1.0
@@ -69,7 +69,7 @@ class El4090Tripod2LowCfg(El4090SpiderCfg):
 
     class commands(El4090SpiderCfg.commands):
         curriculum = True
-        max_curriculum = 3.0
+        max_curriculum = 2.5
         # default: lin_vel_x, lin_vel_y, ang_vel_yaw, heading (in heading mode ang_vel_yaw is recomputed from heading error)
         num_commands = 4
         resampling_time = 4.  # time before command are changed[s]
